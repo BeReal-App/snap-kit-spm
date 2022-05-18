0cfcb24e2a14e07ec3fc66b8562071638afd3a0d
Allan Vialatte <PoissonBallon@users.noreply.github.com>
2022-05-18 17:10:07 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "SnapKit",
+    name: "SnapChatKit",
     platforms: [
         .iOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SnapKit",
+            name: "SnapChatKit",
             targets: ["SCSDKCoreKit", "SCSDKLoginKit", "SCSDKCreativeKit"])
     ],
     targets: [
